7f4be2bda44bc404c29b87f81128284d9b179b55
Danny Yoo <dyoo@cs.wpi.edu>
2011-09-25 13:45:47 -0400

    extending the example so it shows that the mechanism for adding additional infix operators is open

--- example.rkt
+++ example.rkt
@@ -1,5 +1,6 @@
 #lang s-exp "my-lang.rkt"
-(require rackunit)
+(require rackunit
+         (for-syntax racket/base))
 
 (define x 42)
 
@@ -9,6 +10,17 @@
 
 (x := (x + 50))
 (check-equal? x 93)
+
+;; We can label - as being an infixer as well.
+(define-infix-transformer - 
+  (lambda (stx) 
+    (syntax-case stx () 
+      [(lhs op rhs)
+       (syntax/loc stx (- lhs rhs))])))
+
+
+(x := (x - 1))
+(check-equal? x 92)
 
 ;; This should raise an exception, because the + here masks the one
 ;; from our language, the one that's been labeled explicitly as
